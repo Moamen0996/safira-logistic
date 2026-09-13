3b0c8487fba0bb74b7da4a1afcd03a3804823017
Moamen0996 <moamenbeliever@gmail.com>
2026-09-13 12:18:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,14 +13,16 @@ app.use(express.static(path.join(__dirname)));
 
 if (MONGO_URI) {
     mongoose.connect(MONGO_URI)
-        .then(() => console.log('MongoDB Connected'))
+        .then(() => console.log('MongoDB Connected Successfully'))
         .catch(err => console.error('Connection Error:', err));
+} else {
+    console.log('Warning: MONGO_URI is not defined in environment variables.');
 }
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log('Server running on port ' + PORT);
 });
