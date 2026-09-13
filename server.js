de24261ea284e006d51ae7ebf578410e5dc80be8
Moamen0996 <moamenbeliever@gmail.com>
2026-09-13 12:14:40 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const MONGO_URI = process.env.MONGO_URI || '';
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname)));
+
+if (MONGO_URI) {
+    mongoose.connect(MONGO_URI)
+        .then(() => console.log('MongoDB Connected'))
+        .catch(err => console.error('Connection Error:', err));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on port ' + PORT);
+});
