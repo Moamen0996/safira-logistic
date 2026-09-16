2bb6494f9a77064e325e6916a3343b152f8a3506
Moamen0996 <moamenbeliever@gmail.com>
2026-09-16 12:32:09 +0300

    Update server.js

--- public/server.js
+++ public/server.js
@@ -58,4 +58,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-app.get('/test', (req, res) => res.send('Railway is working!'));
